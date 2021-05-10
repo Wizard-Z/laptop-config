b5af588846ce450a252a99dd81cb4d137f073dfa
S RASHWIN NONDA <40497804+nondarashwin@users.noreply.github.com>
2021-05-10 10:18:55 +0530

    Update hyworks.sh

--- hyworks.sh
+++ hyworks.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install p7zip-full p7zip-rar
 sudo apt-get install vim
-sudo 7za x HyWorksClient_Installer_FULL+UPGRADE_328373_XUBUNTU-16.04_x64.7z
-cd edcinstaller
+sudo 7za x HC.7z
+cd HC/edcinstaller
 sudo ./install_edcclient.sh
