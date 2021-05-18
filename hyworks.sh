33abdd5c026139fc5b5365bc77872368c3c5fe20
S RASHWIN NONDA <40497804+nondarashwin@users.noreply.github.com>
2021-05-18 15:40:49 +0530

    Update hyworks.sh

--- hyworks.sh
+++ hyworks.sh
@@ -1,9 +1,12 @@
 echo "select internet is availiable/Enabled radio when propmpted"  
 echo "select Desktop Mode"
+
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install p7zip-full p7zip-rar
 sudo apt-get install vim
-sudo 7za x HC.7z
+echo "run these commands after running the script in same folder as installer is present"
+echo "sudo 7za x HC.7z
 cd HC/edcinstaller
-sudo ./install_edcclient.sh
+sudo ./install_edcclient.sh"
+
