7b78511de07a8df369d8dad2843ad8b2e98590d3
S RASHWIN NONDA <40497804+nondarashwin@users.noreply.github.com>
2021-05-18 13:53:23 +0530

    Update install.sh
    docker installation changes

--- install.sh
+++ install.sh
@@ -30,19 +30,20 @@ echo "************mongo compass installed************"
 echo
 
 echo "************docker installation************"
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#sudo apt-get install \
+#    apt-transport-https \
+#    ca-certificates \
+#    curl \
+#    gnupg \
+#    lsb-release
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 # Adding repo.
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+#echo \
+#  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 # to run docker as non root.
+sudo apt-get install docker.io
 sudo groupadd docker
 echo "Adding user"
 sudo usermod -aG docker $USER
