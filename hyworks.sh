bde3da2b3c6f696c58ef1328dcb543b0619e86f9
Sourabh <38776962+Wizard-Z@users.noreply.github.com>
2021-07-08 09:17:57 +0530

    Update hyworks.sh

--- hyworks.sh
+++ hyworks.sh
@@ -6,7 +6,7 @@ sudo apt-get upgrade
 sudo apt-get install p7zip-full p7zip-rar
 sudo apt-get install vim
 echo "run these commands after running the script in same folder as installer is present"
-sudo 7za x HyWorksClient_Installer_FULL+UPGRADE_328373_XUBUNTU-16.04_x64\ .7z;
+sudo 7za x HyWorksClient_Installer\ .7z;
 cd edcinstaller;
 sudo ./install_edcclient.sh"
 
